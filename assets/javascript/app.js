ec9ffeb0d4ef930b4b7d5d10b63f686ce48a2492
Sangriale <senora.pelos@gmail.com>
2020-03-15 11:59:48 -0500

    work on buttons in JavaScript

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,31 +1,62 @@
 $(document).ready(function () {
 
 
-var televisionShows = ["Supernatural", "Friends", "The Flash", "Arrow", "The Chilling Adventures of Sabrina", "Doctor Who", "Legacy"]    
+var televisionShows = ["Supernatural","The Flash", "Arrow", "The Chilling Adventures of Sabrina", "Doctor Who", "Legacy", "The Mandolorian"]    
 
 
 // function to display Television Show info from the API
 
-function display() {
+function displayInfo() {
     $("#televison-view").empty();
     var topic = $(this).attr("data-name");
-    var queryURL = "https://api.giphy.com/v1/gifs/search?" + televisionShows + "api_key=ZMDyyJt0yDpWObBq4SEvE8f8YENUS7fz&q=&limit=10&offset=0&rating=PG&lang=en";
+    var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=ZMDyyJt0yDpWObBq4SEvE8f8YENUS7fz&q" + televisionShows + "=&limit=10&offset=0&rating=PG&lang=en";
 
     $.ajax ({
         url: queryURL,
         method: "GET"
-    }).then(function(response){
+    })
+    
+    .then(function(response){
+
     if (response.pagination.total_count ===0) {
         var itemIndex = televisionShows.indexOf(topic);
         if (itemIndex > -1) {
+            topic.splice(itemIndex, 1);
             renderButtons();
         }
     }
+   var results = repsonse.data;
+   for(var i = 0; j<results.length; i++){
+       var newShows = $("<div class='television-name'>");
+       var gifRating = $("<p>").text("Rating: " + results[i].rating.toUpperCase());
+        var gifTitle = $("<p>").text("Title:  " + results[i].title.toUpperCase());
+        var gif = $("<img>");
+        gif.attr("src", gifURL);
+        gif.attr("data-still", results[i].images.fixed_height_still.url);
+        gif.attr("data-animate", resultsi[i].images.fixed_heigth.url);
+        gif.attr('data-state', 'still');
+        gif.addClass('animate-gif');
    
+   
+        newShows.append(gifRating);
+        newShows.append(gifTitle);
+        newShows.append(gif);
+
+        $("#television-view").prepend(newShows);
+    }
    
     });
 
+    function renderButtons(){
+        $(".button-view").empty();
 
+      for (var i = 0; i < televisionShows.length; i++){
+          var makeButtons =$('<button>');
+          makeButtons.addClass('televisionShows btn btn-info');
+          makeButtons.attr('data-name', televisionShows[i]);
+          $('.buttons-view').append(makeButtons); 
+      }  
+    }
 
 
 }
